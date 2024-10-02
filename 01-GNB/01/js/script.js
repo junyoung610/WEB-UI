e6433902a544b352e8183acba2589aad3cd225e8
junyoung610 <junyoungkim610@gmail.com>
2024-10-02 13:57:57 +0900

    구현완료

--- 01-GNB/01/js/script.js
+++ 01-GNB/01/js/script.js
@@ -2,13 +2,17 @@ $(function () {
     // 대상을 변수에 저장
     const $menu = $(".gnb > li");
     const $submenu = $(".submenu");
+    const duration = 300; // 0.3초
 
     // 메뉴에 마우스가 들어왔을 때
-    $menu.on("mouseevent", function () {
-        $submenu.slideDown();
+    $menu.on("mouseenter", function () {
+        // .stop() : 현재 진행 중인 애니메이션 즉시 중지
+        $(this).find($submenu).stop().slideDown(duration);
+        $(this).addClass("on");
     });
     // 메뉴에 마우스가 나갔을 때
     $menu.on("mouseleave", function () {
-        $submenu.slideUp();
+        $submenu.stop().slideUp(duration);
+        $menu.removeClass("on");
     });
 });
